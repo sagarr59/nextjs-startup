b1e30d0dd5211ba7cd993bfbda5388af4e2f1895
Sagar Raut <sagarraut2059@gmail.com>
2025-01-19 00:03:07 +0545

    route groups

--- app/(dashboard)/dashboard/users/page.tsx
+++ app/(dashboard)/dashboard/users/page.tsx
@@ -7,9 +7,18 @@ const page = () => {
       <ul className="mt-10">
         <li>
           <Link href="/dashboard/users/1">User 1</Link>
-          {/* users clickable */}
-          <Link href="/dashboard/users/2">User 2</Link>
+        </li>
+        {/* users clickable */}
+        <li>
+          {" "}
+          <Link href="/dashboard/users/2">User 2</Link>{" "}
+        </li>
+        <li>
+          {" "}
           <Link href="/dashboard/users/3">User 3</Link>
+        </li>
+        <li>
+          {" "}
           <Link href="/dashboard/users/4">User 4</Link>
         </li>
       </ul>
